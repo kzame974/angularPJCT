355c3a5117c60f923c7009687dc677930256d88f
Kzame <ludovic.surel@gmail.com>
2020-05-25 21:30:37 +0400

    ajout first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppareilComponent } from './appareil/appareil.component';
+import { MonPremierComponent } from './mon-premier/mon-premier.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppareilComponent,
+    MonPremierComponent
   ],
   imports: [
     BrowserModule,
